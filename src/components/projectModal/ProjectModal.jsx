d9f8c7e1989ba743034809073ca2e02369c97b92
developer-tvc <developer@techversantinfo.com>
2024-07-25 06:36:51 +0530

    cntrol slides

--- src/components/projectModal/ProjectModal.jsx
+++ src/components/projectModal/ProjectModal.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+"use-client"
+import React, { useEffect, useRef, useState } from 'react';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import { Carousel } from 'react-responsive-carousel';
@@ -8,6 +9,8 @@ import CustomLightbox from '../lightbox/LightBox';
 const ProjectModal = ({ open, onClose, project }) => {
   const [lightboxOpen, setLightboxOpen] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const totalSlides = project.images.length; 
 
   const handleImageClick = (index) => {
     setCurrentIndex(index);
@@ -19,19 +22,44 @@ const ProjectModal = ({ open, onClose, project }) => {
   };
 
   const handlePrevSlide = () => {
-    setCurrentIndex((currentIndex - 1 + project.images.length) % project.images.length);
+    setCurrentIndex((currentIndex - 1 + totalSlides) % totalSlides);
   };
 
   const handleNextSlide = () => {
-    setCurrentIndex((currentIndex + 1) % project.images.length);
+    setCurrentIndex((currentIndex + 1) % totalSlides);
   };
+
+  const carouselRef = useRef(null);
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'ArrowRight') {
+      // Move to the next slide
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % totalSlides);
+    } else if (event.key === 'ArrowLeft') {
+      // Move to the previous slide
+      setCurrentSlide((prevSlide) => (prevSlide - 1 + totalSlides) % totalSlides);
+    }
+  };
+
+  useEffect(() => {
+    // Add event listener
+    document.addEventListener('keydown', handleKeyDown);
+
+    // Clean up event listener on component unmount
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
   return (
     <div>
-      <Modal style={{zIndex:2}} open={open} onClose={onClose}>
+      <Modal style={{ zIndex: 2 }} open={open} onClose={onClose}>
         <Box className="flex flex-col lg:flex-row items-center bg-white rounded-lg p-4 outline-none w-3/4 mx-auto mt-20">
           <div className="w-full lg:w-2/3 ">
-            <Carousel showThumbs={false} onClickItem={handleImageClick}>
+            <Carousel
+              selectedItem={currentSlide}
+              onChange={(index) => setCurrentSlide(index)}
+              ref={carouselRef} showThumbs={false} onClickItem={handleImageClick}>
               {project.images.map((image, index) => (
                 <div key={index}>
                   <img src={image} alt={`Project image ${index + 1}`} className="cursor-pointer " />
